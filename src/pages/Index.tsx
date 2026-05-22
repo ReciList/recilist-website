a5e7b66e1ebd5211f7ea67dc462bb3fbb314df65
Marcos Tello <marcostello@MacBook-Air-de-Marcos.local>
2026-05-22 18:47:56 -0300

    feat: remove CookConfidenceSection and FinalCtaSection from landing page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,10 +3,8 @@ import ScrollHero from "@/components/landing/ScrollHero";
 import AiRecipesSection from "@/components/landing/AiRecipesSection";
 import ShoppingListSection from "@/components/landing/ShoppingListSection";
 import NutritionSection from "@/components/landing/NutritionSection";
-import CookConfidenceSection from "@/components/landing/CookConfidenceSection";
 import ProblemSection from "@/components/landing/ProblemSection";
 import PricingSection from "@/components/landing/PricingSection";
-import FinalCtaSection from "@/components/landing/FinalCtaSection";
 import DownloadSection from "@/components/landing/DownloadSection";
 import Footer from "@/components/landing/Footer";
 
@@ -17,13 +15,12 @@ const Index = () => (
     <AiRecipesSection />
     <ShoppingListSection />
     <NutritionSection />
-    <CookConfidenceSection />
     <ProblemSection />
     <PricingSection />
-    <FinalCtaSection />
     <DownloadSection />
     <Footer />
   </div>
 );
 
 export default Index;
+
